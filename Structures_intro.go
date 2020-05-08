e11ebae566f281139f50ff8d8cf9079428d79880
Dipan Chandra <dipanchandra@yahoo.com>
2020-05-09 04:10:38 +0530

    Create Structures_intro.go

---
+++ Structures_intro.go
@@ -0,0 +1,15 @@
+/*
+Go is not an OO Language like C++ / Java etc.
+It does not has objects nor inheritances and hence no polymorphism and overloading.
+
+Go, on the other hand have "structures", which can be asociated with "Methods" and "Composition".
+Definition of a "Structure" and "Class"are verymuch similar.
+*/
+
+type Voter struct {
+  Name string
+  Age int
+}
+
+/* Now, we need to add "Method" to this structure, just like you'dhave methods as a part of "class".
+
